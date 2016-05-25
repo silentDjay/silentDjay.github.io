d1f929357b183e7fc508e59e7f4bff762348c929
Jeremy Smith <smithjeremym@gmail.com>
2016-05-25 16:08:04 -0400

    improve consistency and behavior and look of project info sections

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,14 +3,14 @@ $(document).ready( function () {
 
 
   $(".projDiv").on("mouseover", function () { // when any iconDiv is mousovered...
-    $(this).children().filter(".projTitle").attr("id", "iconHover");
+    // $(this).children().filter(".projTitle").attr("id", "iconHover");
     $(this).children().filter(".line").addClass("lineHover");
     // $(this).children().filter(".projDeets").addClass("englishHover");
     $(this).children().filter(".projDeets").addClass("deutschHover");
   });
 
   $(".projDiv").on("mouseout", function () { // when any iconDiv is mousouted...
-    $(this).children().filter(".projTitle").attr("id", "");
+    // $(this).children().filter(".projTitle").attr("id", "");
     $(this).children().filter(".line").removeClass("lineHover");
     // $(this).children().filter(".english").removeClass("englishHover");
     $(this).children().filter(".projDeets").removeClass("deutschHover");
