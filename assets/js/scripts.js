a481106bb4e47d30d5fa68f12109056891b13339
Jeremy Smith <smithjeremym@gmail.com>
2016-06-02 10:26:10 -0400

    refine copy further. Include more space between header links

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,18 +1,13 @@
 $(document).ready( function () {
   "use strict";
 
-
-  $(".projDiv").on("mouseover", function () { // when any iconDiv is mousovered...
-    // $(this).children().filter(".projTitle").attr("id", "iconHover");
+  $(".projDiv").on("mouseover", function () {
     $(this).children().filter(".line").addClass("lineHover");
-    // $(this).children().filter(".projDeets").addClass("englishHover");
     $(this).children().filter(".projDeets").addClass("deutschHover");
   });
 
-  $(".projDiv").on("mouseout", function () { // when any iconDiv is mousouted...
-    // $(this).children().filter(".projTitle").attr("id", "");
+  $(".projDiv").on("mouseout", function () {
     $(this).children().filter(".line").removeClass("lineHover");
-    // $(this).children().filter(".english").removeClass("englishHover");
     $(this).children().filter(".projDeets").removeClass("deutschHover");
   });
 
